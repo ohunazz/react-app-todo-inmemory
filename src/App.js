25ef28e3391aeeb73de3949a1ee4313f039dcbea
ohunazz <ohunazz@gmail.com>
2023-12-14 17:48:04 -0500

    Add addTodo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
 import React from "react";
+import { v4 as uuid } from "uuid";
 import "./App.css";
 
-export class App extends React.Component {
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: [],
+            inputValue: ""
+        };
+    }
+
+    addTodo = (e) => {
+        e.preventDefault();
+        console.log(this.state.inputValue);
+        const newTodo = {
+            id: uuid(),
+            text: this.state.inputValue,
+            isDone: false
+        };
+
+        this.setState((prevState) => {
+            const copyTodos = [...prevState.todos, newTodo];
+            return {
+                todos: copyTodos,
+                inputValue: ""
+            };
+        });
+    };
+
+    handleOnChange = (e) => {
+        const { value } = e.target;
+        this.setState({
+            inputValue: value
+        });
+    };
+
     render() {
-        return <main></main>;
+        return (
+            <main>
+                <form onSubmit={this.addTodo}>
+                    <input
+                        onChange={this.handleOnChange}
+                        value={this.state.inputValue}
+                        type="text"
+                        placeholder="What is your mind"
+                    />
+                    <input type="submit" value="Add Todo" />
+                </form>
+            </main>
+        );
     }
 }
+
+export default App;
